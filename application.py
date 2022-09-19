82fea6889caae3b1a2b7f246526440e45e18c7fe
iamjavaexpert <techexpert7140@gmail.com>
2022-09-19 12:09:24 +0530

    change application name

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ from SWx_Dailly_View_Project import create_app
 
 env_name = os.getenv('FLASK_ENV')
 
-app = create_app(env_name=env_name)
+application = create_app(env_name=env_name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
