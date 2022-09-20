254f09c1a142fef28aca9971fb782f06f1400266
iamjavaexpert <techexpert7140@gmail.com>
2022-09-20 18:05:59 +0530

    Add Flask-Cors and initialize it

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import os
 
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 from SWx_Dailly_View_Project import create_app
 
@@ -9,6 +10,14 @@ load_dotenv()
 env_name = os.getenv('FLASK_ENV')
 
 application = create_app(env_name=env_name)
+
+CORS(application, origins=[
+    "http://localhost",
+    "http://localhost:*",
+    "http://127.0.0.1:*",
+    "http://0.0.0.0:*",
+    "http://swxbackenddev-env.eba-qddq4zxt.us-east-1.elasticbeanstalk.com"
+])
 
 if __name__ == '__main__':
     application.run(debug=True)
