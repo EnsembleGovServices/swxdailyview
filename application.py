8373a970fcf3d0ecdad90b0884589c89867b48a9
iamjavaexpert <techexpert7140@gmail.com>
2022-09-21 15:21:27 +0530

    remove origins from cors

--- application.py
+++ application.py
@@ -11,13 +11,7 @@ env_name = os.getenv('FLASK_ENV')
 
 application = create_app(env_name=env_name)
 
-CORS(application, origins=[
-    "http://localhost",
-    "http://localhost:*",
-    "http://127.0.0.1:*",
-    "http://0.0.0.0:*",
-    "http://swxbackenddev-env.eba-qddq4zxt.us-east-1.elasticbeanstalk.com"
-])
+cors = CORS(application)
 
 if __name__ == '__main__':
     application.run(debug=True)
