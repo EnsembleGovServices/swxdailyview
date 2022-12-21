b6d56c16840a695f522f6eaf433fa58087ec207c
iamjavaexpert <techexpert7140@gmail.com>
2022-12-21 11:47:42 +0530

    testing: modified with dynamic time_stamp fetching

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
 import os
+import time
+from datetime import date, timedelta
+
 import pytest
 from dotenv import load_dotenv
 from SWx_Dailly_View_Project import create_app
@@ -35,127 +38,137 @@ def access_data():
     return access_key, secret_key, bucket_name
 
 
+current_date = date.today()
+last_one_day = current_date - timedelta(days=1)
+
+tuple = last_one_day.timetuple()
+last_date_timestamp = int(str(time.mktime(tuple)).split('.')[0])
+desired_time_stamps = []
+desired_time_stamps.append(last_date_timestamp)
+for i in range(23):
+    desired_time_stamps.append(last_date_timestamp+3600)
+    last_date_timestamp+=3600
 @pytest.fixture()
 def time_stamp():
-    time_stamp_value = 1671527941
+    time_stamp_value = desired_time_stamps[0]
     return time_stamp_value
 
 @pytest.fixture()
 def time_stamp_13hour():
-    time_stamp_value = 1671521825
+    time_stamp_value = desired_time_stamps[13]
     return time_stamp_value
 
 @pytest.fixture()
 def time_stamp_12hour():
-    time_stamp_value = 1671518225
+    time_stamp_value = desired_time_stamps[12]
     return time_stamp_value
 @pytest.fixture()
 def time_stamp_10hour():
-    time_stamp_value = 1671511025
+    time_stamp_value = desired_time_stamps[10]
     return time_stamp_value
 @pytest.fixture()
 def time_stamp_11hour():
-    time_stamp_value = 1671514625
+    time_stamp_value = desired_time_stamps[11]
     return time_stamp_value
 
 @pytest.fixture()
 def time_stamp_9hour():
-    time_stamp_value = 1671507425
+    time_stamp_value = desired_time_stamps[9]
     return time_stamp_value
 
 @pytest.fixture()
 def time_stamp_8hour():
-    time_stamp_value = 1671503825
+    time_stamp_value = desired_time_stamps[8]
     return time_stamp_value
 
 @pytest.fixture()
 def time_stamp_7hour():
-    time_stamp_value = 1671500225
+    time_stamp_value = desired_time_stamps[7]
     return time_stamp_value
 
 @pytest.fixture()
 def time_stamp_6hour():
-    time_stamp_value = 1671496625
+    time_stamp_value = desired_time_stamps[6]
     return time_stamp_value
 
 @pytest.fixture()
 def time_stamp_5hour():
-    time_stamp_value = 1671493025
+    time_stamp_value = desired_time_stamps[5]
     return time_stamp_value
 
 @pytest.fixture()
 def time_stamp_4hour():
-    time_stamp_value = 1671489425
+    time_stamp_value = desired_time_stamps[4]
     return time_stamp_value
 
 @pytest.fixture()
 def time_stamp_3hour():
-    time_stamp_value = 1671485825
+    time_stamp_value = desired_time_stamps[3]
     return time_stamp_value
 
 @pytest.fixture()
 def time_stamp_2hour():
-    time_stamp_value = 1671482225
+    time_stamp_value = desired_time_stamps[2]
     return time_stamp_value
 
 @pytest.fixture()
 def time_stamp_1hour():
-    time_stamp_value = 1671478625
+    time_stamp_value = desired_time_stamps[1]
     return time_stamp_value
 
 @pytest.fixture()
 def time_stamp_0hour():
-    time_stamp_value = 1671475025
+    time_stamp_value = desired_time_stamps[0]
     return time_stamp_value
 
 @pytest.fixture()
 def time_stamp_14hour():
-    time_stamp_value = 1671525425
+    time_stamp_value = desired_time_stamps[14]
     return time_stamp_value
 
 @pytest.fixture()
 def time_stamp_15hour():
-    time_stamp_value = 1671529025
+    time_stamp_value = desired_time_stamps[15]
     return time_stamp_value
 
 @pytest.fixture()
 def time_stamp_16hour():
-    time_stamp_value = 1671532625
+    time_stamp_value = desired_time_stamps[16]
     return time_stamp_value
 
 @pytest.fixture()
 def time_stamp_17hour():
-    time_stamp_value = 1671536225
+    time_stamp_value = desired_time_stamps[17]
     return time_stamp_value
 
 @pytest.fixture()
 def time_stamp_18hour():
-    time_stamp_value = 1671539825
+    time_stamp_value = desired_time_stamps[18]
     return time_stamp_value
 
 @pytest.fixture()
 def time_stamp_19hour():
-    time_stamp_value = 1671543425
+    time_stamp_value = desired_time_stamps[19]
     return time_stamp_value
 
 @pytest.fixture()
 def time_stamp_20hour():
-    time_stamp_value = 1671547025
+    time_stamp_value = desired_time_stamps[20]
     return time_stamp_value
 
 @pytest.fixture()
 def time_stamp_21hour():
-    time_stamp_value = 1671550625
+    time_stamp_value = desired_time_stamps[21]
     return time_stamp_value
 
 @pytest.fixture()
 def time_stamp_22hour():
-    time_stamp_value = 1671554225
+    time_stamp_value = desired_time_stamps[22]
     return time_stamp_value
 
 @pytest.fixture()
 def time_stamp_23hour():
-    time_stamp_value = 1671557793
+    time_stamp_value = desired_time_stamps[23]
     return time_stamp_value
 
 time_list = [ time_stamp, time_stamp_0hour, time_stamp_1hour,
